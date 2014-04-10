68aac968e6ba7eefb061f701ae83154b73a894a5
Rachel Keirouz <rkeirouz@ucsd.edu>
2014-04-10 10:04:07 -0700

    Made interactive

--- lib/Hello.java
+++ lib/Hello.java
@@ -1,14 +1,12 @@
+import java.util.Scanner;
 public class Hello
 {
     public static void main( String argv[] )
     {
-       //Default is "World"
-       //Author: Rachel (rkeirouz@ucsd.edu)
-       String name = "World";
-        if ( argv.length != 0 )
-        {
-            name = argv[0];
-        }
+        System.out.println("What's your name");
+        Scanner in = new Scanner(System.in);
+        String my_name = in.nextLine();
 
-        System.out.println( "Hello, " + name + "!" );    }
+        System.out.println( "Hello, " + my_name + "!" );
+    }
 }
